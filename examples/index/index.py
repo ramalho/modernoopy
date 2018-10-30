6460147f1fc46f7b3b5c6ed1f550cc29d109da42
Luciano Ramalho <luciano@ramalho.org>
2018-10-29 21:59:57 -0300

    added inverted index sample

---
+++ examples/index/index.py
@@ -0,0 +1,14 @@
+import collections
+
+
+class Index:
+    """An inverted index. It maps each key to a set of values."""
+
+    def __init__(self):
+        self._map = collections.defaultdict(set)
+
+    def add(self, key, value):
+        self._map[key.upper()].add(value)
+
+    def __getitem__(self, key):
+        return self._map[key.upper()]
---
+++ examples/index/test_index.py
@@ -0,0 +1,36 @@
+from index import Index
+
+
+def test_unique_entry():
+    idx = Index()
+    idx.add("COLON", ":")
+    assert idx["colon"] == {":"}
+
+
+def test_three_occurrences():
+    sample = [("7", "DIGIT"), ("8", "DIGIT"), ("9", "DIGIT")]
+    idx = Index()
+    for char, word in sample:
+        idx.add(word, char)
+    assert idx["digit"] == {"7", "8", "9"}
+
+
+def test_several_entries():
+    sample = [
+        ("7", "DIGIT SEVEN"),
+        ("8", "DIGIT EIGHT"),
+        ("9", "DIGIT NINE"),
+        (":", "COLON"),
+        (";", "SEMICOLON"),
+        ("<", "LESS-THAN SIGN"),
+        ("=", "EQUALS SIGN"),
+        (">", "GREATER-THAN SIGN"),
+    ]
+    idx = Index()
+    for char, words in sample:
+        for word in words.split():
+            idx.add(word, char)
+    assert idx["digit"] == {"7", "8", "9"}
+    assert idx["nine"] == {"9"}
+    assert idx["sign"] == {"<", "=", ">"}
+    assert idx["no-such-entry"] == set()
