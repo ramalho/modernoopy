3f83b59ef5384a2c46fbc830fd486841785ea7d2
Luciano Ramalho <luciano@ramalho.org>
2019-01-31 15:59:15 -0200

    minor refactoring of functional bingo example

--- examples/bingo/funbingo.py
+++ examples/bingo/funbingo.py
@@ -1,18 +1,18 @@
 """
 Function ``funbingo`` returns a closure that picks an item
 at random from a collection of items, removing the item
-from the collection, like to a bingo cage.
+from the collection, like a bingo cage.
 
 To create a loaded instance, provide an iterable with the items::
 
     >>> balls = set(range(3))
-    >>> enclosure = funbingo(balls)
+    >>> cage = funbingo(balls)
 
 Invoking the closure retrieves an item at random, removing it::
 
-    >>> b1 = enclosure()
-    >>> b2 = enclosure()
-    >>> b3 = enclosure()
+    >>> b1 = cage()
+    >>> b2 = cage()
+    >>> b3 = cage()
     >>> balls == {b1, b2, b3}
     True
 
