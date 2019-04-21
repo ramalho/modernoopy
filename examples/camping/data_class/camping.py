dfa2ce7530e2cd98ee351182ec7c0bc71659dd94
Luciano Ramalho <luciano@ramalho.org>
2019-04-21 17:29:27 -0300

    refactored two-class versions of camping.py

--- examples/camping/data_class/camping.py
+++ examples/camping/data_class/camping.py
@@ -17,14 +17,13 @@ class Camper:
     def pay(self, amount):
         self.paid += amount
 
-    def display(self, individual_share):
-        camper_tpl = '{name:>{width}} paid ${paid:7.2f}, balance: $ {balance:7.2f}'
-        print(camper_tpl.format(
+    def display(self):
+        camper_tpl = '{name:>{width}} paid ${paid:7.2f}'
+        return camper_tpl.format(
             name = self.name,
             width = self.max_name_len,
             paid = self.paid,
-            balance = self.paid - individual_share,
-        ))
+        )
 
 class Budget:
     """
@@ -48,16 +47,13 @@ class Budget:
     def individual_share(self):
         return self.total() / len(self._campers)
 
-    def summaries(self):
-        share = self.individual_share()
-        return [camper.summary() for camper in self._campers.values()]
-
     def report(self):
         """report displays names and amounts due or owed"""
         share = self.individual_share()
-        heading_tpl = "Total: ${:6.2f}; individual share: ${:6.2f}"
+        heading_tpl = 'Total: ${:6.2f}; individual share: ${:6.2f}'
         print(heading_tpl.format(self.total(), share))
         print("-"* 42)
         sorted_campers = sorted(self._campers.values(), key=operator.attrgetter('paid'))
         for camper in sorted_campers:
-            camper.display(share)
+            balance = f'balance: $ {camper.paid - share:7.2f}'
+            print(camper.display(), balance, sep=', ')
