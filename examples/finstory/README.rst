b14f331acc9a8a113f24fb65fd64e78ce442b27c
Luciano Ramalho <luciano@ramalho.org>
2019-04-23 16:14:40 -0300

    finstory and deducstory examples

--- examples/finstory/README.rst
+++ examples/finstory/README.rst
@@ -3,7 +3,8 @@ Financial History Example
 
 ``FinancialHistory`` instances keep track of a person's expenses and income.
 
-.. note::  This example is adapted from *Smalltalk-80: the language*, by Adele Goldberg and Dave Robson (Addison-Wesley, 1989). 
+.. note::  This example is adapted from *Smalltalk-80: the language*,
+           by Adele Goldberg and Dave Robson (Addison-Wesley, 1989).
 
 The interface of ``FinancialHistory`` consists of:
 
@@ -27,3 +28,50 @@ The interface of ``FinancialHistory`` consists of:
 
 ``spent_for(reason)``
     Return total amount spent for the given reason.
+
+
+Demonstration
+-------------
+
+Create ``FinancialHistory`` with $ 100.
+
+    >>> from finstory import FinancialHistory
+    >>> h = FinancialHistory(100)
+    >>> h
+    <FinancialHistory balance: 100.00>
+
+Spend some money::
+
+    >>> h.spend(39.95, 'meal')
+    >>> h
+    <FinancialHistory balance: 60.05>
+
+Decimals can be formatted like floats::
+
+    >>> print(f'${h.balance:0.2f}')
+    $60.05
+
+Get more money::
+
+    >>> h.receive(1000.01, "Molly's game")
+    >>> h.receive(10.00, 'found on street')
+    >>> h
+    <FinancialHistory balance: 1070.06>
+
+Spend more money::
+
+    >>> h.spend(55.36, 'meal')
+    >>> h.spend(26.65, 'meal')
+    >>> h.spend(300, 'concert')
+    >>> h
+    <FinancialHistory balance: 688.05>
+
+Check amount spent on meals::
+
+    >>> h.spent_for('meal')
+    Decimal('121.96')
+
+Check amount spent on travel (zero):
+
+    >>> h.spent_for('travel')
+    Decimal('0')
--- examples/finstory/finstory.py
+++ examples/finstory/finstory.py
@@ -1,120 +1,43 @@
-"""
-``FinancialHistory`` keeps track of a person's expenses,
-income, and balance.
-
-Create ``FinancialHistory`` with $ 100.
-
-    >>> h = FinancialHistory('100')
-    >>> h
-    <FinancialHistory(100.00): 0 transactions>
-
-Spend some money::
-
-    >>> h.spend('39.95', 'meal')
-    >>> h
-    <FinancialHistory(60.05): 1 transaction>
-    >>> h.balance
-    Decimal('60.05')
-
-Decimals can be formatted like floats::
-
-    >>> print(f'${h.balance:0.2f}')
-    $60.05
-
-Get more money::
-
-    >>> h.receive(1000.01, "Molly's game")
-    >>> h.receive(10.01, 'found on street')
-    >>> h
-    <FinancialHistory(1070.07): 3 transactions>
-
-Spend more money::
-
-    >>> h.spend(55.35, 'meal')
-    >>> h.spend(26.65, 'meal')
-    >>> h.spend(300, 'concert')
-    >>> h
-    <FinancialHistory(688.07): 6 transactions>
-
-Check amount spent on meals::
-
-    >>> h.spent_for('meal')
-    Decimal('121.95')
-
-Check amount spent on travel (zero):
-
-    >>> h.spent_for('travel')
-    Decimal('0')
-
-Statement:
-
-    >>> for line in h.statement():  # doctest: +NORMALIZE_WHITESPACE
-    ...   print(line)
-    # initial balance    +100.00
-    meal                  -39.95
-    Molly's game        +1000.01
-    found on street       +10.01
-    meal                  -55.35
-    meal                  -26.65
-    concert              -300.00
-    # current balance    +688.07
-
-
-"""
-
 import collections
 import decimal
 
-
 decimal.setcontext(decimal.BasicContext)
 
-
-def clean_decimal(value):
+def new_decimal(value):
     """Builds a Decimal using the cleaner float `repr`"""
     if isinstance(value, float):
         value = repr(value)
     return decimal.Decimal(value)
 
 
-Transaction = collections.namedtuple("Transaction", "amount description")
-
-
 class FinancialHistory:
 
-    def __init__(self, amount=0):
-        self._initial_balance = clean_decimal(amount)
-        self._history = []
+    def __init__(self, initial_balance=0.0):
+        self._balance = new_decimal(initial_balance)
+        self._incomes = collections.defaultdict(decimal.Decimal)
+        self._expenses = collections.defaultdict(decimal.Decimal)
 
     def __repr__(self):
-        bal = self.balance
-        len_hist = len(self._history)
-        plural = 's' if len_hist != 1 else ''
-        return f'<FinancialHistory({bal:0.2f}): {len_hist} transaction{plural}>'
-
-    def receive(self, amount, source):
-        amount = clean_decimal(amount)
-        self._history.append(Transaction(amount, source))
-
-    def spend(self, amount, reason):
-        amount = -clean_decimal(amount)
-        self._history.append(Transaction(amount, reason))
+        name = self.__class__.__name__
+        return f'<{name} balance: {self._balance:.2f}>'
 
     @property
     def balance(self):
-        return self._initial_balance + sum(t.amount for t in self._history)
+        return self._balance
+
+
+    def receive(self, amount, source):
+        amount = new_decimal(amount)
+        self._incomes[source] += amount
+        self._balance += amount
+
+    def spend(self, amount, reason):
+        amount = new_decimal(amount)
+        self._expenses[reason] += amount
+        self._balance -= amount
+
+    def received_from(self, source):
+        return self._incomes[source]
 
     def spent_for(self, reason):
-        select = (t.amount for t in self._history if t.description == reason)
-        return -sum(select, clean_decimal(0))
-
-    def statement(self):
-        line_fmt = '{reason:12}\t{value:+12.2f}'
-        balance = self._initial_balance
-        msg = '# initial balance'
-        yield line_fmt.format(reason=msg, value=balance)
-        for t in self._history:
-            yield line_fmt.format(reason=t.description,
-                                  value=t.amount)
-            balance += t.amount
-        msg = '# current balance'
-        yield line_fmt.format(reason=msg, value=balance)
+        return self._expenses[reason]
